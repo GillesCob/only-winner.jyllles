4a0241dc0e5ac2341c167461526946932cce00d2
Gilles <104164728+GillesCob@users.noreply.github.com>
2024-03-15 15:55:14 +0100

    toute la chaine fonctionne

--- second_rename.py
+++ second_rename.py
@@ -11,10 +11,12 @@ Feuille_datas = str(actual_day)
 too_long = 1
 
 # Chemin vers le fichier Excel
-chemin_excel = f'/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/EXCEL/DATAS.xlsx'
+excel_month_file_path = f'/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/EXCEL/DATAS 2.xlsx'
+excel_BDD_INITIALE_file_path = f'/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/BDD INITIALE.xlsx'
+
 
 # Lire les données Excel en utilisant pandas depuis la feuille du jour
-df = pd.read_excel(chemin_excel, sheet_name=Feuille_datas)
+df1 = pd.read_excel(excel_month_file_path, sheet_name=Feuille_datas)
 
 # Dossiers contenant les images initiales, tous les NFT et enfin ceux nouvellement créés
 dossier_initial = f"/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/PNG_DIAPO/cartes_avec_images"
@@ -30,7 +32,7 @@ for nom_diapo in os.listdir(dossier_initial):
         nom_diapo = nom_diapo[:-4]  # Retirer l'extension ".png"
         
         # Vérifier si le nom du fichier correspond à une valeur de la colonne A
-        for index, row in df.iterrows():
+        for index, row in df1.iterrows():
             if nom_diapo == str(row['Nom image']):
                 
                 # Obtenir la valeur correspondante de la colonne "Prompt"
@@ -46,9 +48,13 @@ for nom_diapo in os.listdir(dossier_initial):
                 chemin_fichier_renomme = os.path.join(os.path.dirname(dossier_initial),nouveau_nom)
                 os.rename(chemin_complet_diapo,chemin_fichier_renomme)
                 
+                
                 dossier_import_WP_JOUR = os.path.join(dossier_WP, (actual_day + " " + scrapping_month))
                 os.makedirs(dossier_import_WP_JOUR, exist_ok=True)
                 
                 shutil.copy(chemin_fichier_renomme, os.path.join(dossier_NFT,nouveau_nom))
+                print("Image envoyée dans le dossier NFT")
+                
                 shutil.move(chemin_fichier_renomme, os.path.join(dossier_import_WP_JOUR,nouveau_nom))
+                print("Image envoyée dans le dossier pour l'import WP")
             

9891be39e01cedd5b70a712afc76f80210d54db5
Gilles <104164728+GillesCob@users.noreply.github.com>
2024-03-23 12:30:56 +0100

    Ajout de la feuille pour importer le produit dans WOO

--- old/first_rename.py
+++ old/first_rename.py
@@ -1,6 +1,60 @@
-from html2image import Html2Image
+import os
+import pandas as pd
 from datetime import datetime
+import openpyxl
+
+from html2image import Html2Image
 from openpyxl import load_workbook
+
+
+#ATTENTION : BIEN LIRE LES INFOS SUIVANTES : 
+#Va transformer la syntaxe Midjourney en Prompt présents dans l'excel
+#Attention à bien mettre à jour le mois concerné !
+
+scrapping_month = "Mars"
+actual_day = int(datetime.now().day)
+excel_sheet = str(actual_day)
+
+
+# Chemin vers le fichier Excel
+chemin_excel = f'/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/EXCEL/DATAS.xlsx'
+# Lire les données Excel en utilisant pandas
+df = pd.read_excel(chemin_excel, sheet_name=excel_sheet)
+# Chemin vers le dossier contenant les images
+dossier_images = "/Users/gillescobigo/Downloads"
+# Chemin vers le dossier contenant les images renommées
+dossier_images_renamed = f"/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/IMAGES_MIDJOURNEY"
+
+
+classeur_month = openpyxl.load_workbook(chemin_excel)
+
+excel_sheet_informations = classeur_month[excel_sheet]
+#Je créé une liste avec les prompts Midjourney
+prompt_midjourney_in_excel = [cell.value for cell in excel_sheet_informations['N'] if cell.value is not None]
+#Idem avec les noms des cartes
+cards_name_in_excel = [cell.value for cell in excel_sheet_informations['M'] if cell.value is not None]
+
+# Parcourir les fichiers dans le dossier d'images
+for nom_fichier in os.listdir(dossier_images):
+    chemin_fichier = os.path.join(dossier_images, nom_fichier)
+
+    # Vérifier si le fichier est un fichier PNG
+    if nom_fichier.lower().endswith(".png"):
+        nom_fichier = nom_fichier[:60]
+        if nom_fichier in prompt_midjourney_in_excel:
+            index_prompt = prompt_midjourney_in_excel.index(nom_fichier)
+            nouveau_nom = cards_name_in_excel[index_prompt]
+            nouveau_nom = nouveau_nom + ".png"
+            os.rename(chemin_fichier, os.path.join(dossier_images_renamed, nouveau_nom))
+            
+            prompt_midjourney_in_excel.pop(index_prompt)
+            cards_name_in_excel.pop(index_prompt)
+            print(f"Nom changé de {nom_fichier} en {nouveau_nom}")
+        else :
+            print(f"{nom_fichier} n'est pas dans les prompts Midjourney fournis dans l'excel")
+
+
+
 
 hti = Html2Image(size=(1000,1500))
 
@@ -76,7 +130,6 @@ for index, competition in enumerate(COMP_OF_SPORT_list) :
     nft_name = NOM_NFT_list[index]
     #Chemin vers les images Midjourney
     image_midjourney_path = f'/Users/gillescobigo/Documents/Gilles/Dev/Only Winners/DATAS/2024/{scrapping_month}/IMAGES_MIDJOURNEY/{nft_name}.png'
-    print(comp_of_sport)
     
     
     # Modèle de HTML avec CSS
@@ -111,7 +164,7 @@ for index, competition in enumerate(COMP_OF_SPORT_list) :
                 position: absolute;
                 width: 975px;
                 height: 1475px;
-                background: linear-gradient(to top right, #C59833, #73581A);
+                background: linear-gradient(to top right, #d8aa3f, #927021);
             }}
 
             .competition {{
